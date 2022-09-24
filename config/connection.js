31d055a342ccbb168b440672c8523b2566d23a30
Alex Bellamy <alexjbellamy@gmail.com>
2022-09-24 13:51:13 -0400

    wrote schema for tracker_db database

--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,4 @@
+require('dotenv').config(); // used to store db credentials in a local .env file that won't be pushed to the repo
 const mysql = require('mysql2');
 
 // Creating a mySQL connection using environment variables stored in .env - requires dotenv node module
--- index.js
+++ index.js
@@ -1,2 +1,1 @@
-require('dotenv').config(); // used to store db credentials in a local .env file that won't be pushed to the repo
 const db = require('./config/connection');
